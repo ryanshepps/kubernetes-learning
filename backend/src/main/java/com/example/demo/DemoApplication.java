2186e470eb33499fee4e75bd3494ff93277a583a
Ryan Sheppard <sheppardryan@rogers.com>
2023-10-10 18:33:19 -0400

    add liveness and readiness probes

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep((long)(Math.random() * 60000)); // Waits up to 60 seconds
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
